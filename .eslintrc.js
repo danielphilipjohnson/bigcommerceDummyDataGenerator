47150b8a290a68dcfe26ee8e31f4d1d3d742f304
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-11-08 20:52:36 +0000

    removed unused package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,24 @@
 module.exports = {
-	root: true,
-	env: {
-	  node: true
-	},
-	extends: [
-	  'eslint:recommended',
-	  '@vue/typescript/recommended',
-	  'plugin:prettier/recommended'
-	],
-	parserOptions: {
-	  ecmaVersion: 2020
-	},
-	rules: {
-	  'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-	  'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-	  'arrow-parens': 'off',
-	  'eol-last': 'error'
-	},
-	overrides: [
-	  {
-		files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
-		env: {
-		  jest: true
-		}
-	  }
-	]
-  }
-  
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'arrow-parens': 'off',
+    'eol-last': 'error'
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
